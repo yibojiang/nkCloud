c2e59ea6f7de4b65ececef9df604748586a03156
yibojiang <yibo.jiang@coconutislandstudio.com>
2014-01-09 14:23:34 +0800

    first commit

---
+++ cloud/main.js
@@ -0,0 +1,11 @@
+// Use AV.Cloud.define to define as many cloud functions as you want.
+// For example:
+AV.Cloud.define("hello", function(request, response) {
+  response.success("Hello world!");
+});
+
+AV.Cloud.define("GetDateTime",function(request,response){
+	var d1=new Date();
+	//response.success(d1.toString('yyyy-MM-dd'));
+	response.success(d1);
+});
